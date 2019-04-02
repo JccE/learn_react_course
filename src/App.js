36b574a6ef22e8d3069fd89e61af40d4f9c285bd
jcce <toshajo99@gmail.com>
2019-04-02 15:52:03 -0500

    styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 
+import './App.css';
+
+
+// explains a few ways of putting styles into react
+
+
+
 class App extends Component {
+
+
   render() {
+    const name = "Jimmy"
     return (
       <div className="App">
-         React
+         <button>Button 1</button><br/>
+         React <br/><br/>
+         {name}
       </div>
     );
   }
+
+  // name = "Jimmy"
+  // render() {
+  //   return (
+  //     <div className="App">
+  //        React <br/><br/>
+  //        {this.name}
+  //     </div>
+  //   );
+  // }
 }
+
+
+const Appinstance = new App()
+
+console.log(Appinstance)
+
+
+
 
 export default App;
