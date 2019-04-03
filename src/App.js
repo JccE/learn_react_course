7a063b931dcff0ceb04e79a3e5ab909c43727530
jcce <toshajo99@gmail.com>
2019-04-02 19:37:49 -0500

    conditionals via functions

--- src/App.js
+++ src/App.js
@@ -6,17 +6,37 @@ import Container1 from './containers/container1';
 
 class App extends Component {
 
+  renderFunction1 = () => {
+    if(true) {
+      return(
+        <div>
+          Condition 1
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          Condition 2
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div>
         React
-      {/*nested ternary expression*/}
-        {false
-          ? false ? <div>Condition 1 </div> : <div> Condition 2 </div>
-          : true ? <div>Condition 3 </div> : <div> Condition 4 </div>
-        }
+
+      <div>
+        Div 1
       </div>
-      )
+      <div>
+        Div 2
+      </div>
+      {this.renderFunction1()}
+      </div>
+    )
   }
 }
     // if(false) {
